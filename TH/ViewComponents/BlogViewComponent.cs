9ef2b22a7dd3b1472cfa90e5102ef547f266eae6
AnhQuoc8683 <nkaquoc2002@gmail.com>
2024-11-04 16:35:59 +0700

    Add project files.

---
+++ TH/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using TH.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+namespace TH.ViewComponents
+{
+    public class BlogViewComponent : ViewComponent
+    {
+        public readonly HarmicContext _context;
+        public BlogViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult>InvokeAsync()
+        {
+            var items = _context.TbBlogs.Include(m => m.Category)
+                .Where(m=>(bool)m.IsActive);
+            return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m=>m.BlogId).ToList()));
+        }
+    }
+}
