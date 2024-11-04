91e8bd8cdf3759d3852daa0e59ca9b510cf04bd6
AnhQuoc8683 <nkaquoc2002@gmail.com>
2024-11-04 17:04:48 +0700

    firrts

---
+++ ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using TH.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+namespace TH.ViewComponents
+{
+	public class ProductViewComponent : ViewComponent
+	{
+		private readonly HarmicContext _context;
+		public ProductViewComponent(HarmicContext context)
+		{
+			_context = context;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = _context.TbProducts.Include(m=>m.CategoryProduct)
+				.Where(m=>(bool)m.IsActive).Where(m=>m.IsNew);
+			return await Task.FromResult<IViewComponentResult>(View(items.OrderBy(m=>m.ProductId).ToList()));
+		}
+	}
+}
